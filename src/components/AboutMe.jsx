f62b30e3e3820838055013df1bdbac68093f49b1
Alexisb34 <alexis.bengel@epitech.eu>
2024-10-15 11:21:18 +0200

    delette Animated text deprecied

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,5 @@
 import React, { forwardRef } from "react";
 import { saveAs } from "file-saver";
-import AnimatedText from 'react-animated-text-content';
 import { API_URL } from "../config";
 import { AiOutlineLinkedin } from "react-icons/ai";
 import photoPortrait from '../assets/images/portrait.jpg';
@@ -17,22 +16,13 @@ const AboutMe = (props,ref) => {
   return (
     
     <div className = "block justify-center items-center fadeIn"> 
-      <div className="justify-center text-center pt-28">
+      <div className="justify-center pt-28">
         <div className="block">
           <div className="lg:flex justify-center">
             <img className= "rounded-lg m-auto mb-10 block h-72 lg:mt-20 lg:m-10  fadeIn"src= {photoPortrait} alt="portrait"/>
             <div>      
               <span ref={ref} className="text-my-yellow lg:text-4xl text-2xl lg:pr-48 font-canvasBlack">Qui suis-je<br/></span>
-              <AnimatedText
-                className="text-white m-auto pt-10 w-5/6  pb-24 max-w-md"
-                type='chars'
-                interval={0.02}
-                duration={0.04}
-                animation={{
-                  ease: 'ease',
-                  scale: 1.68,
-                }}
-              >
+              <p className="text-white w-[300px] pt-8">
                 Bonjour, je m'appelle Alexis j'ai 32 ans et je suis originaire du sud
                 de la France.
                 Je me suis installé récemment à Nantes pour entreprendre une reconversion
@@ -42,7 +32,8 @@ const AboutMe = (props,ref) => {
                 Dans la vie j'aime faire du Softball et du Baseball.
                 J'adore sortir avec des amis et leur faire découvrir ma passion pour les 
                 boissons houblonnées. Il m'arrive même d'en brasser moi-même. 
-              </AnimatedText>
+
+              </p>
             </div> 
           </div>
           <div className="flex justify-center">
